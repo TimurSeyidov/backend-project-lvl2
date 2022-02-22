c79cb98678874aafe16ace9faa0d7b0fe4c6e7e2
Тимур Сейидов <timursejidov@MacBook-Pro-Timur.local>
2022-02-22 22:02:57 +0500

    coverate fixed

--- src/gendiff.js
+++ src/gendiff.js
@@ -14,27 +14,27 @@ const getFileData = (file) => {
 };
 
 const analyze = (obj1 = {}, obj2 = {}) => {
-  const lines = [];
+  let lines = [];
   const keys = _.uniq([].concat(Object.keys(obj1), Object.keys(obj2))).sort();
   keys.forEach((key) => {
     const data1 = _.get(obj1, key);
     const data2 = _.get(obj2, key);
-    let ins = {};
+    let ins = [];
     if (_.isUndefined(data2)) {
-      ins = { prefix: '-', value: !_.isObject(data1) ? data1.toString() : analyze(data1, data1) };
+      ins = [{ key, prefix: '-', value: !_.isObject(data1) ? data1.toString() : analyze(data1, data1) }];
     } else if (_.isUndefined(data1)) {
-      ins = { prefix: '+', value: !_.isObject(data2) ? data2.toString() : analyze(data2, data2) };
+      ins = [{ key, prefix: '+', value: !_.isObject(data2) ? data2.toString() : analyze(data2, data2) }];
     } else if (_.isObject(data1) && _.isObject(data2)) {
-      ins = { prefix: ' ', value: analyze(data1, data2) };
+      ins = [{ key, prefix: ' ', value: analyze(data1, data2) }];
     } else if (data1 === data2) {
-      ins = { prefix: ' ', value: data1 };
-    }
-    if (Object.keys(ins).length) {
-      lines.push(Object.assign(ins, { key }));
+      ins = [{ key, prefix: ' ', value: data1 }];
     } else {
-      lines.push({ prefix: '-', key, value: _.isObject(data1) ? analyze(data1, data1) : data1 });
-      lines.push({ prefix: '+', key, value: _.isObject(data2) ? analyze(data2, data2) : data2 });
+      ins = [
+        { prefix: '-', key, value: _.isObject(data1) ? analyze(data1, data1) : data1 },
+        { prefix: '+', key, value: _.isObject(data2) ? analyze(data2, data2) : data2 },
+      ];
     }
+    lines = [].concat(lines, ins);
   });
   return lines;
 };
